b40ac7aea84a57d49baa9f456673d4fdf2be9590
BoghdanRohach <73877085+BoghdanRohach@users.noreply.github.com>
2021-01-26 01:07:19 +0200

    added new test

--- cypress/integration/navigations.spec.js
+++ cypress/integration/navigations.spec.js
@@ -3,31 +3,31 @@
 context('Navigations tests', () => {
 
     beforeEach(() => {
-      cy.visit('https://www.its.uz.ua');
-      cy.viewport(1980, 1024);
+        cy.visit('https://www.its.uz.ua');
+        cy.viewport(1980, 1024);
     })
-  
+
 
     it('As user i can navigate to about us page', () => {
         cy
-        .get('.navbar-custom a[href="/about/"]')
-        .should('have.text', 'Про Нас')
-        .click();
-        cy.title().should('eq',"Про нас | its.uz.ua");
+            .get('.navbar-custom a[href="/about/"]')
+            .should('have.text', 'Про Нас')
+            .click();
+        cy.title().should('eq', "Про нас | its.uz.ua");
         cy
-        .get('.container>h2')
-        .should('have.text', 'УЖГОРОДСЬКА ШКОЛА ТЕСТУВАННЯ ПРОГРАМНОГО ЗАБЕЗПЕЧЕННЯ')
+            .get('.container>h2')
+            .should('have.text', 'УЖГОРОДСЬКА ШКОЛА ТЕСТУВАННЯ ПРОГРАМНОГО ЗАБЕЗПЕЧЕННЯ')
     })
 
-    it('As user I can find link by text and navigate to page', ()=>{
+    it('As user I can find link by text and navigate to page', () => {
         cy.contains('Про Нас').click()
-        cy.title().should('eq',"Про нас | its.uz.ua");
+        cy.title().should('eq', "Про нас | its.uz.ua");
     })
 
-    it('As user when I go to contact and fill form I can click send and my message will sended', ()=>{
+    it('As user when I go to contact and fill form I can click send and my message will sended', () => {
         cy
-        .contains('Контакти')
-        .click()
+            .contains('Контакти')
+            .click()
 
         cy.get('.contact h2').should('have.text', "Залиште нам повідомлення!");
         cy.get("#name").type('Andriy').should('have.value', 'Andriy')
@@ -37,4 +37,14 @@ context('Navigations tests', () => {
         cy.get('#button').click();
     })
 
+
+    it('As user i can navigate to courses page', () => {
+        cy.contains('Курси').click()
+        cy.title().should('eq', "Курси | its.uz.ua");
+        cy.get('.container>h2').should('have.text', "ОБЕРТЬ КУРС ЯКИЙ ВИ ХОЧЕТЕ ПРОЙТИ");
+        cy.get('.srvice-text a[href="/services/base_qa/"]').should('have.text', 'РУЧНЕ ТЕСТУВАННЯ');
+        cy.get('.srvice-text a[href="/services/automation_testing/"]').should('have.text', 'АВТОМАТИЗОВАНЕ ТЕСТУВАННЯ');
+        cy.get('.srvice-text a[href="/services/music_it/"]').should('have.text', 'МУЗИЧНА ГРАМОТА ДЛЯ ІТ');
+        cy.get('.srvice-text a[href="/services/pjm/"]').should('have.text', 'Практичний IT проект менеджмент');
+    })
 })
